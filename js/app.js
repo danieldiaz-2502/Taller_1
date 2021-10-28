93af602b4fc2aeb3a215969dda6957d1d4fc7245
danieldiaz-2502 <daniel_diazvalencia@outlook.com>
2021-10-28 16:50:43 -0500

    order filter

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 const products = [
     {
         name: "Bufanda Blanket",
-        price: "120,000 COP",
+        priceTag: "120,000 COP",
+        price: 120000,
         image: "./img/product_1.png",
         description: "Nuevo - Bufanda tipo blanket",
         type: "scarfs"
@@ -9,7 +10,8 @@ const products = [
 
     {
         name: "Bufanda Tul",
-        price: "70,000 COP",
+        priceTag: "70,000 COP",
+        price: 70000,
         image: "./img/product_2.png",
         description: "Nuevo - Bufanda de tul mágico",
         type: "scarfs"
@@ -17,7 +19,8 @@ const products = [
 
     {
         name: "Falda Tul",
-        price: "130,000 COP",
+        priceTag: "130,000 COP",
+        price: 130000,
         image: "./img/product_4.png",
         description: "Nuevo - Falda de tul mágico",
         type: "skirts"
@@ -34,7 +37,7 @@ const productTemplate = (item) => {
     product.innerHTML = `
     <img src="${item.image}" alt="" class="product__img">
     <div class="product__description">
-        <h3 class="product__price">${item.price}</h3>
+        <h3 class="product__price">${item.priceTag}</h3>
         <p class="product__tag">${item.description}</p>
         <h3>MÁS COLORES</h3>
         <button class="product__cart">Agregar al carrito</button>
@@ -57,22 +60,45 @@ const productTemplate = (item) => {
 
 const filterByCategory = document.getElementById("categories");
 
+const orderBySelect = document.getElementById("orderBy");
+
+orderBySelect.addEventListener("change", e => {
+    loadProducts();
+})
+
 filterByCategory.addEventListener("change", e => {
+
+    loadProducts();
+
+});
+
+const loadProducts = () => {
     const category = filterByCategory.value;
+    const order = orderBySelect.value;
     productsSection.innerHTML = "";
+
     let filteredProductsByCategory;
 
-    if(category !== ""){
+    if (category !== "") {
         filteredProductsByCategory = products.filter((product) =>
-        product.type === category);
+            product.type === category);
     } else {
         filteredProductsByCategory = products;
+    }
+
+    if (order === "asc"){
+        filteredProductsByCategory = filteredProductsByCategory.sort((a,b) => 
+        a.price - b.price);
+    }
+    if (order === "desc"){
+        filteredProductsByCategory = filteredProductsByCategory.sort((a,b) => 
+        b.price - a.price);
     }
 
     filteredProductsByCategory.forEach(product => {
         productTemplate(product);
     });
-});
+}
 
 products.forEach(product => {
     productTemplate(product);
