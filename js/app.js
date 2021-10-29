e1c797be7e0825cfa26ccb9135333a02c524e61d
danieldiaz-2502 <daniel_diazvalencia@outlook.com>
2021-10-28 23:28:31 -0500

    products and button

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const products = [
     {
+        id: 1,
         name: "Bufanda Blanket",
         priceTag: "120,000 COP",
         price: 120000,
@@ -9,6 +10,7 @@ const products = [
     },
 
     {
+        id: 2,
         name: "Bufanda Tul",
         priceTag: "70,000 COP",
         price: 70000,
@@ -18,12 +20,40 @@ const products = [
     },
 
     {
+        id: 4,
         name: "Falda Tul",
         priceTag: "130,000 COP",
         price: 130000,
         image: "./img/product_4.png",
         description: "Nuevo - Falda de tul mágico",
         type: "skirts"
+    },
+    {
+        id: 3,
+        name: "Bufanda de tela",
+        priceTag: "95,000 COP",
+        price: 95000,
+        image: "./img/product_3.png",
+        description: "Nuevo - Bufanda de tela",
+        type: "scarfs"
+    },
+    {
+        id: 5,
+        name: "Pantalon de tela",
+        priceTag: "100,000 COP",
+        price: 100000,
+        image: "./img/product_5.png",
+        description: "Nuevo - Pantalones de lana mágica",
+        type: "pants"
+    },
+    {
+        id: 6,
+        name: "Leggins",
+        priceTag: "70,000 COP",
+        price: 70000,
+        image: "./img/product_6.png",
+        description: "Nuevo - Leggins de tela mágica",
+        type: "pants"
     },
 ];
 
@@ -32,15 +62,30 @@ const cart = [];
 const productsSection = document.getElementById("products");
 
 const productTemplate = (item) => {
+
     const product = document.createElement("a");
+
     product.className = "product";
+
+    product.setAttribute("href", `./product.html?id=${item.id}`);
+
+    const isAdded = cart.some(product => product.id === item.id);
+    let buttonHtml;
+
+    if (isAdded) {
+        buttonHtml = `<button class="product__cart" disabled>Añadido</button>`;
+    } else {
+        buttonHtml = `<button class="product__cart">Agregar al carrito</button>`;
+    }
+
     product.innerHTML = `
     <img src="${item.image}" alt="" class="product__img">
     <div class="product__description">
         <h3 class="product__price">${item.priceTag}</h3>
         <p class="product__tag">${item.description}</p>
         <h3>MÁS COLORES</h3>
-        <button class="product__cart">Agregar al carrito</button>
+
+        ${buttonHtml}
     </div>
     `;
     productsSection.appendChild(product);
@@ -50,11 +95,13 @@ const productTemplate = (item) => {
     productCart.addEventListener("click", e => {
         e.preventDefault();
         const productAdded = {
+            id: item.id,
             name: item.name,
             image: item.image,
         };
         cart.push(productAdded);
-        console.log(productAdded);
+        console.log(cart);
+        productCart.setAttribute("disabled", true);
     });
 }
 
@@ -86,13 +133,13 @@ const loadProducts = () => {
         filteredProductsByCategory = products;
     }
 
-    if (order === "asc"){
-        filteredProductsByCategory = filteredProductsByCategory.sort((a,b) => 
-        a.price - b.price);
+    if (order === "asc") {
+        filteredProductsByCategory = filteredProductsByCategory.sort((a, b) =>
+            a.price - b.price);
     }
-    if (order === "desc"){
-        filteredProductsByCategory = filteredProductsByCategory.sort((a,b) => 
-        b.price - a.price);
+    if (order === "desc") {
+        filteredProductsByCategory = filteredProductsByCategory.sort((a, b) =>
+            b.price - a.price);
     }
 
     filteredProductsByCategory.forEach(product => {
